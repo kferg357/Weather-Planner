6977e6a1c29f4fe5023c0093f56f90368fb3d877
kferg357 <kennethferguson90@gmail.com>
2021-05-02 18:20:02 -0400

    added java and index

---
+++ Assets/javascript.js
@@ -0,0 +1,27 @@
+
+
+var cityInput = document.querySelector("#city");
+var cityForm = document.querySelector("#city-search-form");
+var pastSearchButton = document.querySelector("#past-search-button");
+var weatherContainer = document.querySelector("#cur-weather-container");
+var citySearchInput = document.querySelector("searched-cty");
+var forcastTitle = document.querySelector("#forecast");
+var forecastContainerEl = document.querySelector("#fiveday-container");
+var pastSearchButtonEl = document.querySelector("#past-search-buttons");
+
+
+
+// <script>
+// $(document).ready(function() {
+//   $.ajax({
+//     url: "https://maps.openweathermap.org/maps/2.0/weather/{op}/{z}/{x}/{y}&appid={API key}",
+//     type: "GET",
+//     success: function(result) {
+//       console.log(result);
+//     },
+//     error: function(error) {
+//       console.log(error);
+//     }
+//   });
+// });
+</script>
