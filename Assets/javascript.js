4cbcabcababfc45fb9402e0acc661db16ae58056
kferg357 <kennethferguson90@gmail.com>
2021-05-03 19:27:37 -0400

    added java

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -1,24 +1,41 @@
 
 
+
+
+
 var cityInput = document.querySelector("#city");
 var cityForm = document.querySelector("#city-search-form");
 var pastSearchButton = document.querySelector("#past-search-button");
 var weatherContainer = document.querySelector("#cur-weather-container");
 var citySearchInput = document.querySelector("city-input");
 var forcastTitle = document.querySelector("#forecast");
-var forecastContainerEl = document.querySelector("#fiveday-container");
-var pastSearchButtonEl = document.querySelector("#past-search-buttons");
+var forecastContainer = document.querySelector("#fiveday-container");
+var pastSearchButton = document.querySelector("#past-search-buttons");
 
+var requestUrl = 'api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}';
 
-var savedSearch = function() {
+fetch(requestUrl)
+then(function (data) {
+    return data.json();
+     }
+     .then(function (data) {
+         for (var i =5; i < data.length; i++)
+     }
+
+var savedSearch = function () {
     localStorage.setItem("city", JSON.stringify(cities));
 };
-console.log('cities')
+console.log('city')
 
- 
+
+
+fetch(requestUrl)
+    .
+
+
 // $(document).ready(function() {
 //   $.ajax({
-//     url: "api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}",
+//     url: ,
 //     type: "GET",
 //     success: function(result) {
 //       console.log(result);
