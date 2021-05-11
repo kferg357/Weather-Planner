6ec4e6df90147fcf05b3fc140d2dd51ee6697cc0
kferg357 <kennethferguson90@gmail.com>
2021-05-11 11:23:27 -0400

    added

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -8,16 +8,13 @@ var pastSearchButton = document.querySelector('#past-search-buttons');
 var API_KEY = '10e1f68a65cde5b6f69c3c18e862cb60';
 var city_search = '';
 var requestUrl = `https://openweathermap.org/api/geocoding-api#direct_name`;
-var citybuttons = $("#city-buttons");
+var citybuttons = $('#city-buttons');
 var current_timestamp = new Date();
-var today = moment().format("dddd, MMMM Do YYYY");
+var today = moment().format('dddd, MMMM Do YYYY');
 console.log(`today is ${today}`);
 var tomorrow = moment().add(1, 'day');
 console.log(`tomorrow is ${tomorrow}`);
-getcities()
-
-
-
+getcities();
 cityForm.addEventListener('submit', function (event) {
   event.preventDefault();
   city_search = cityInput.value;
@@ -26,45 +23,45 @@ cityForm.addEventListener('submit', function (event) {
      event.preventDefault();
      city_search = cityInput.value;
      console.log("city_search now storing: ", city_search)
-   
      weatherSearch();
    });
    */
-
-
-
   weatherSearch();
 });
-
-
-
 var savedSearch = function (city_search) {
- var nameof = localStorage.setItem(city_search,JSON.stringify("Greensboro"))
-  console.log(nameof)
+  var priorSearches = getcities() || [];
+  priorSearches.push(city_search);
+  localStorage.setItem('city_search', JSON.stringify(priorSearches));
+};
+function getcities() {
+  var test = JSON.parse(localStorage.getItem('city_search'));
+  console.log(test);
+  return test;
 }
-
-
-function getcities() { 
- var test = JSON.parse (localStorage.getItem('city_search'))
- console.log(test)
- }
-
+function addSearchButton(cityName) {
+  var nameDiv = $('<button>');
+  nameDiv.append(cityName);
+  nameDiv.addClass('namecities');
+  citybuttons.append(nameDiv);
+}
+function addOldSearchButtonsOnPageLoad() {
+  citiesSearched = getcities();
+  if (citiesSearched.length > 0) {
+    for (var i = 0; i < citiesSearched.length; i++) {
+      addSearchButton(citiesSearched[i]);
+    }
+  }
+}
+addOldSearchButtonsOnPageLoad();
 function weatherSearch() {
   var cityName = $('#city').val();
   var coordinatesUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${API_KEY}`;
-
-
   // Clear out any old data on the DOM
   // //
-  forecastContainer.innerHTML = ""
-  pastSearchButton.innerHTML = ""
-  
-  var nameDiv = $("<button>");
-  nameDiv.append(cityName);
-  nameDiv.addClass("namecities");
-  citybuttons.append(nameDiv);
-
-
+  forecastContainer.innerHTML = '';
+  pastSearchButton.innerHTML = '';
+  addSearchButton(cityName);
+  savedSearch(cityName);
   fetch(coordinatesUrl)
     .then(function (data) {
       return data.json();
@@ -76,27 +73,21 @@ function weatherSearch() {
       // test lat and lon
       var lat = data[0].lat;
       var lon = data[0].lon;
-      
       console.log(lat, 'lat');
       console.log(lon, 'lon');
       var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${API_KEY}`;
       fetch(apiUrl)
         .then(function (data) {
-          console.log(data.json)
-          
-
+          console.log(data.json);
           return data.json();
         })
         .then(function (weatherdata) {
-          
-           
-          $('#cur-forecast').empty()
+          $('#cur-forecast').empty();
           var curWeatherContainer = $('#cur-forecast');
-          var weatherDiv = $("<div>");
-          weatherDiv.addClass("weatherstats");
+          var weatherDiv = $('<div>');
+          weatherDiv.addClass('weatherstats');
           weatherDiv.append(`<h3>${data[0].name}</h3>`);
-          weatherDiv.append(`<h3>${today}</h3>`);                  
-          
+          weatherDiv.append(`<h3>${today}</h3>`);
           console.log('curWeatherContainer:', curWeatherContainer);
           weatherDiv.append(`<content>Temperature ${weatherdata.current.temp}</content>`);
           weatherDiv.append(`<content>UV Index ${weatherdata.current.uvi}</content>`);
@@ -106,39 +97,31 @@ function weatherSearch() {
 
 
           
-
-
-
-          //   fivedayForcasst.append(`h2>Temperature ${data.ftemp}</h2>`);
-          // function k2f(K) {
-          //   return Math.floor((K - 273.15) * 1.8 + 32);
-          // }
-
           for (var i = 0; i < 5; i++) {
-            console.log(moment().add(i + 1, 'day').format('dddd, MMMM Do'))
+            console.log(
+              moment()
+                .add(i + 1, 'day')
+                .format('dddd, MMMM Do')
+            );
             console.log(weatherdata.daily[i].temp.day);
-            console.log(weatherdata.daily[i].wind_speed)
-            console.log(weatherdata.daily[i].uvi)
-            console.log(weatherdata.daily[i].humidity)
+            console.log(weatherdata.daily[i].wind_speed);
+            console.log(weatherdata.daily[i].uvi);
+            console.log(weatherdata.daily[i].humidity);
             var temp = `<div class= "c" >
-            <p>Date ${moment().add(i + 1, 'day').format('dddd, MMMM Do')}</p>
+            <p>Date ${moment()
+              .add(i + 1, 'day')
+              .format('dddd, MMMM Do')}</p>
             <p>Temperature ${weatherdata.daily[i].temp.day}</p>
             <p>UV Index ${weatherdata.daily[i].uvi}</p>
             <p>Wind Speed ${weatherdata.daily[i].wind_speed}</p>
             <p>Humidtiy ${weatherdata.daily[i].humidity}</p>
-            </div>`
-
-            var forecastContainer = $("#fiveday-container");
-            forecastContainer.append(temp)
+            </div>`;
+            var forecastContainer = $('#fiveday-container');
+            forecastContainer.append(temp);
             //  forecastContainer.append(`<p>Temperature ${data.daily[i].temp.day}</p>`);
             //  forecastContainer.append(`<p>UV Index ${data.daily[i].wind_speed}</p>`);
             //  forecastContainer.append(`<p>Wind Speed ${data.daily[i].uvi}</py>`);
             //  forecastContainer.append(`<p>Humidity ${data.daily[i].humidity}</p>`);
-
-
-
-
-
           }
         });
       //  fetch(forcastapiUrl)
@@ -146,16 +129,9 @@ function weatherSearch() {
       //      return data.json();
       //  })
     });
-
-  // this line 
+  // this line
   // localStorage.setItem();
-
   //     let temp = localStorage.getItem(id);
   //     console.log(temp, 'TEMP');
   //     console.log(localStorage, 'localStorage')
-
 }
-
-
-
-
